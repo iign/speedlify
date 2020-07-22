e251ed3bbf06c8ae9a2e6f6e66bc0f65f3b0edc9
Nacho Toledo <ignacio@ign.uy>
2020-07-22 10:32:44 -0300

    Update netlify.js

--- _data/sites/netlify.js
+++ _data/sites/netlify.js
@@ -1,26 +1,16 @@
 module.exports = {
-	name: "Netlify", // optional, falls back to object key
-	description: "Netlify web sites",
+	name: "GCM", // optional, falls back to object key
+	description: "GCM Network",
 	options: {
 		frequency: 60 * 11 + 30, // 11h, 30m
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
 		// Use "site" if sites are all on the same origin and share assets.
-		freshChrome: "site",
+		freshChrome: "run",
 	},
 	urls: [
-		"https://www.netlify.com/",
-		"https://www.netlify.com/donation-matching/",
-		"https://www.netlify.com/products/build/plugins/",
-		"https://www.netlify.com/with/sitecore/",
-		"https://www.netlify.com/with/drupal/",
-		"https://www.netlify.com/with/wordpress/",
-		"https://www.netlify.com/webinar/a-drupal-journey-to-the-jamstack/",
-		"https://www.netlify.com/partners/agency/",
-		"https://www.netlify.com/careers/",
-		"https://www.netlify.com/pricing/",
-		"https://jamstackconf.com/",
-		"https://jamstackconf.com/virtual/",
+		"https://www.fahrradbook.de/",
+		"https://www.supplementbibel.de/"
 	]
 };
